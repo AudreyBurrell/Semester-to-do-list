83fd821e31ae5f814810253ed6e0316ece383e6e
AudreyBurrell <audreyb2165@gmail.com>
2025-12-12 22:57:45 -0700

    Making the basic layout for the page where the user can choose to add items for the semester or go to to-do list.

--- src/AddOrView.jsx
+++ src/AddOrView.jsx
@@ -1,9 +1,13 @@
+import './AddOrView.css'
 //this page will just display instructions and two buttons: a button that leads to a place where they can manually import or import a CSV for start-of semester planning, and the other button leads straight to the to-do list.
 //Maybe I can add something on the semester planning (like a date where the semester ends) and if the date when the semester ends hasn't passed, it just goes straight to the to-do list part if something has already been made for the semester.
 function AddOrView() {
     return (
-        <div>
-            <h1>Navigated correctly</h1> 
+        <div className = "mainCard">
+            <div className = "buttonContainer">
+                <button id="addItemsBtn"> Add Assignments </button>
+                <button id="goToListBtn"> Go To To-Do List </button>
+            </div>
         </div>
     );
 }
