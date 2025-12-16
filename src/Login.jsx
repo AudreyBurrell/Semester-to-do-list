48813d901c04298c427eaa0e73afc23bbd2354a0
AudreyBurrell <audreyb2165@gmail.com>
2025-12-15 21:53:40 -0700

    Finishing the functionality for the login page minus the whole storing thing.

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,36 @@
-// import './Home.css'
-// import { useNavigate } from 'react-router-dom';
+import './Login.css' 
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 function Login() {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const handleLogin = () => { 
+        if (username.trim() === '' || password.trim() === '') {
+            alert('Please fill in both username and password');
+            return;
+        } else {
+            console.log('Login clicked!')
+            console.log("Username:", username);
+            console.log("Password:", password);
+            navigate('/AddOrView');
+        }
+    };
     return (
-        <div>
-            
+        <div className="mainCard">
+            <p>Login to Assignment Tracker</p>
+            <div className="formArea">
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" name="username" required value={username} onChange={(e) => setUsername(e.target.value)} />
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <button className="loginBtn" onClick={handleLogin}>Login</button>
         </div>
     )
 }
 
 export default Login;
+
+//Later add in storing stuff.
