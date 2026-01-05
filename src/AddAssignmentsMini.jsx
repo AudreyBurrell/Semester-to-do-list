0ebdad4b2ab82566b13bf28e409214881d93cd04
AudreyBurrell <audreyb2165@gmail.com>
2026-01-04 20:27:40 -0700

    Creating the basic structure for the mini add assignments page.

---
+++ src/AddAssignmentsMini.jsx
@@ -0,0 +1,15 @@
+import './AddAssignmentsMini.css'
+import { useState, useRef } from 'react';  
+import { useNavigate, useLocation } from 'react-router-dom'; 
+
+function AddAssignmentsMini() {
+    //stuff can go here
+    return (
+        <div>
+            <h1>Navigated to add assignments mini </h1>
+        </div>
+    )
+
+}
+
+export default AddAssignmentsMini; 
